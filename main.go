c838ed543b521a0211b906cedeb2e83b47cdb6ca
kigongovincent <kigongovincent81@gmail.com>
2025-12-29 10:29:20 +0300

    init

--- main.go
+++ main.go
@@ -6,8 +6,12 @@ import (
 )
 
 func main() {
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "hello from the server")
 	})
+
+	fmt.Println("server started on port 3000")
 	http.ListenAndServe(":3000", nil)
+
 }
